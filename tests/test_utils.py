1f4a85c5548abab23a3c4dc61005af1f0f5a4303
kyoto7250 <50972773+kyoto7250@users.noreply.github.com>
2023-10-08 21:35:37 +0900

    add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from mdpd import utils
 
 
 class TestUtils(unittest.TestCase):
-    def test_from_md(self):
+    def test_simple_pattern_from_md(self):
         df = utils.from_md(
             """
             | Syntax    | Description |
@@ -15,4 +15,116 @@ class TestUtils(unittest.TestCase):
             | Paragraph | Text        |
             """
         )
-        assert (df == pd.read_csv("tests/fixtures/pattern_1.csv")).all().all()
+        assert (df == pd.read_csv("tests/fixtures/simple_pattern.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            +------------+-------------+
+            | Syntax     | Description |
+            +------------+-------------+
+            | Header     | Title       |
+            | Paragraph  | Text        |
+            +------------+-------------+
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/simple_pattern.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            | Syntax    | Description |
+            | :-------- | ----------: |
+            | Header    | Title       |
+            | Paragraph | Text        |
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/simple_pattern.csv")).all().all()
+
+    def test_header_only_from_md(self) -> None:
+        df = utils.from_md(
+            """
+            | Syntax    | Description |
+            | --------- | ----------- |
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/header_only.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            +------------+-------------+
+            | Syntax     | Description |
+            +------------+-------------+
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/header_only.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            | Syntax    | Description |
+            | :-------- | ----------: |
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/header_only.csv")).all().all()
+
+    def test_one_column_from_md(self):
+        df = utils.from_md(
+            """
+            | Syntax    |
+            | --------- |
+            | Header    |
+            | Paragraph |
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/one_column.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            +------------+
+            | Syntax     |
+            +------------+
+            | Header     |
+            | Paragraph  |
+            +------------+
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/one_column.csv")).all().all()
+
+        df = utils.from_md(
+            """
+            | Syntax    |
+            | :-------- |
+            | Header    |
+            | Paragraph |
+            """
+        )
+        assert (df == pd.read_csv("tests/fixtures/one_column.csv")).all().all()
+
+    def test_one_column_header_only_from_md(self):
+        df = utils.from_md(
+            """
+            | Syntax    |
+            | --------- |
+            """
+        )
+        assert (
+            (df == pd.read_csv("tests/fixtures/one_column_header_only.csv")).all().all()
+        )
+        df = utils.from_md(
+            """
+            +------------+
+            | Syntax     |
+            +------------+
+            """
+        )
+        assert (
+            (df == pd.read_csv("tests/fixtures/one_column_header_only.csv")).all().all()
+        )
+
+        df = utils.from_md(
+            """
+            | Syntax    |
+            | :-------- |
+            """
+        )
+        assert (
+            (df == pd.read_csv("tests/fixtures/one_column_header_only.csv")).all().all()
+        )
